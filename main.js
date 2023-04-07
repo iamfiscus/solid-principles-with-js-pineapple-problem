817b6e28397af51358da70518617ea2c09a52389
Jonathan Fiscus <jfiscus@p2wtnwhk9k.lan>
2023-04-07 13:57:24 -0400

    Solid: Pizza, with multiple chefs

--- main.js
+++ main.js
@@ -11,13 +11,76 @@ const chefs = [headChef, assistantChef1, assistantChef2];
 // Move supervise method from Chef to HeadChef
 headChef.supervise(chefs);
 
-
 // Make a Pizza: createPizza method from PizzaFactory to Pizza class
-const pizza1 = pizzaFactory.createPizza("flour", "water", "salt", "tomatoes", "olive oil", "garlic", "mozzarella");
+export const detroit = {
+  dough: {
+    type: "pan",
+    ingredients: ["flour", "water", "salt"],
+    preparation: "mix ingredients and knead dough",
+    bakingTime: 10,
+  },
+  sauce: {
+    type: "red",
+    ingredients: ["tomatoes", "olive oil", "garlic"],
+    preparation: "mix ingredients and simmer on stove",
+    spreadAmount: 0.5,
+  },
+  cheese: {
+    type: "mozzarella",
+    ingredients: ["mozzarella cheese", "salt"],
+    preparation: "shred cheese",
+    spreadAmount: 0.75,
+  },
+  toppings: {
+    type: "detroit",
+    ingredients: ["pepperoni", "sausage"],
+    preparation: "slice and cook meats",
+    spreadAmount: 1,
+  },
+  baking: {
+    temperature: 450,
+    duration: 15,
+  },
+};
+
+const pizza1 = pizzaFactory.createPizza(detroit);
 pizza1.prepare(); // logs "Preparing pizza with flour, water, salt, tomatoes, olive oil, garlic, mozzarella"
 
-const pizza2 = pizzaFactory.createPizza("flour", "water", "salt", "pepperoni", "sausage");
-pizza2.prepare(); // logs "Preparing pizza with flour, water, salt, pepperoni, sausage"
+const hawaiian = {
+  dough: {
+    type: "thin",
+    ingredients: ["flour", "water", "salt"],
+    preparation: "mix ingredients and knead dough",
+    bakingTime: 10,
+  },
+  sauce: {
+    type: "red",
+    ingredients: ["tomatoes", "olive oil", "garlic"],
+    preparation: "mix ingredients and simmer on stove",
+    spreadAmount: 0.5,
+  },
+  cheese: {
+    type: "mozzarella",
+    ingredients: ["milk", "rennet", "salt"],
+    preparation: "heat milk, add rennet and salt, let curdle, and strain curds",
+    spreadAmount: 0.75,
+  },
+  toppings: {
+    type: "hawaiian",
+    ingredients: ["spam", "pineapple"],
+    preparation: "slice and cook meats",
+    spreadAmount: 1,
+  },
+  baking: {
+    temperature: 450,
+    duration: 15,
+  },
+};
 
-const pizza3 = pizzaFactory.createPizza("flour", "water", "salt", "pepperoni", "pineapple");
-// throws an error: "Not all ingredients are available. Pineapple problem solved!"
+// Pineapple problem solved!
+const pizza3 = pizzaFactory.createPizza(hawaiian);
+// throws an Error: The following ingredients are not available: rennet, spam, pineapple
+
+// CHALLENGES  -----
+// @TODO Enhancement: Add or remove ingredients from availableIngredients
+// @TODO Enhancment: Add a method to PizzaFactory to create a pizza from a recipe
